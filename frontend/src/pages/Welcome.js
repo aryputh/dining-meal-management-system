46f712530205e89de528914fee32c5abacd3d587
aryputh <aryanputhran@gmail.com>
2025-04-17 10:46:00 -0700

    Started improving UI

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -7,7 +7,7 @@ const Welcome = ({ setUser }) => {
     return (
         <div>
             <h1>Welcome to the Dining Meal Management System</h1>
-            <button className="primary-btn" onClick={() => setShowPopup(true)}>Login / Register</button>
+            <button className="primary-btn" onClick={() => setShowPopup(true)}>Continue</button>
             {showPopup && <AuthPopup setUser={setUser} closePopup={() => setShowPopup(false)} />}
         </div>
     );
